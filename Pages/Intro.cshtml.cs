6ac1c499f3f16d15776cb1bf4692dac62b4d53b6
sauravarya <A00236910@mycambrian.ca>
2021-06-18 08:34:06 +0530

    added new razor page, link, new section & name h3

---
+++ Pages/Intro.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_Assignment_1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
